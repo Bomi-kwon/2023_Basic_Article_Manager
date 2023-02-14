a4efcb4a433cf7ccfd3a9233c11cc2300f197965
Bomi-kwon <bbomicoding@gmail.com>
2023-02-14 19:43:04 +0900

    게시판 만들기 시작 - exit 기능 만들기

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
 		Scanner sc = new Scanner(System.in);
-		String cmd = sc.nextLine();
-		System.out.println(cmd);
+		
+		while(true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+		}
+		
+		System.out.println("== 프로그램 끝 ==");
+		sc.close();
 	}
 }
